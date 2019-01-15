c74344292ba7c0e16946a5001bae30522a3cabe2
Jessica Alves <jessralves@gmail.com>
2019-01-15 16:10:32 +1100

    Add home components

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Navbar from "./homeComponents/Header";
 
-export default function home(){
+export default function home() {
   return (
     <div>
+      <Navbar />
       <h1>This is home component </h1>
     </div>
   );
