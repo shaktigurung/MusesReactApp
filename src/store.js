7ea562c25d65ce14604744be4a617bcb10db1c36
shaktigurung <mr.shakti.gurung@gmail.com>
2019-01-15 14:01:37 +1100

    Redux with redux thunk

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import indexReducers from './reducers/indexReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(initialState={}) {
  return createStore(
     indexReducers,
-   applyMiddleware(thunk)
+   composeEnhancers(applyMiddleware(thunk))
  );
 }
