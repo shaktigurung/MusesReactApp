02828291770a426e1c2b07d2bf6e49ab7ed658d0
shaktigurung <mr.shakti.gurung@gmail.com>
2019-01-15 14:24:44 +1100

    Redux form setup

--- src/reducers/indexReducer.js
+++ src/reducers/indexReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import simpleReducer from './simpleReducer';
+import {reducer as formReducer} from "redux-form";
 
 export default combineReducers({
- simpleReducer
+ simpleReducer,
+ form: formReducer
 });
