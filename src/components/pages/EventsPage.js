40d093ab1d68f5ad6464ecbd7fd83eadd7937fbb
shaktigurung <mr.shakti.gurung@gmail.com>
2019-01-16 17:04:43 +1100

    Event page on progress

--- src/components/pages/EventsPage.js
+++ src/components/pages/EventsPage.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import axios from "axios";
+require('dotenv').config();
 
 class EventsPage extends Component {
+
+  //Move it to Redux
+  state={
+    events: []
+  };
+
+  getEvents =  async ()=>{
+    const events = await axios.get(`${process.env.REACT_APP_BACK_END_DOMAIN}/events`);
+    console.log(events);
+    this.setState({ events: events.data});
+   
+  }
+
+  componentDidMount(){
+    this.getEvents();
+  }
   render() {
+    const {events} = this.state;
     return (
       <div>
-        <h1>Events Page </h1>
+        <h1> Events Page </h1>
+        {events.map(event => <li key={event._id}>{event.title}</li>)}
+        {/* <p>{allevent}</p> */}
       </div>
     );
   }
