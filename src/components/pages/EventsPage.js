91bb5246e0c81ea8af8cc164ff3cae758bfda796
shaktigurung <mr.shakti.gurung@gmail.com>
2019-01-21 15:24:11 +1100

    Single Event page created

--- src/components/pages/EventsPage.js
+++ src/components/pages/EventsPage.js
@@ -5,34 +5,28 @@ import {connect} from "react-redux";
 import { Container, Row , Col, Button, Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Badge, CardGroup} from 'reactstrap';
  import axios from "axios";
- require('dotenv').config();
-
-
+ //require('dotenv').config();
+ import {withRouter} from "react-router-dom";
 
 class EventsPage extends Component {
   
-  //Move it to Redux
   state={
     events: []
   };
-
-  // getEvents =  async ()=>{
-  //   const events = await axios.get(`${process.env.REACT_APP_BACK_END_DOMAIN}/events`);
-  //   this.setState({ events: events.data});
-  // }
 
   componentDidMount(){
     this.props.getEvents();
   }
 
-  handleClick = async ()=>{
-    const events = await axios.get(`${process.env.REACT_APP_BACK_END_DOMAIN}/events/:id`);
-    this.setState({ events: events.data});
+  handleClick = (id)=>{
+    // alert(`HandleClick is clicked, ${id}`);
+     this.props.history.push(`/events/${id}`);
+  
   }
 
   futureEvents = ()=>{
-    const {events} = this.props;
-    let currentDate = new Date();
+      const {events} = this.props;
+      let currentDate = new Date();
      //Future Events
      return events.filter(function(event){
       const eventDate = new Date(event.date);
@@ -41,8 +35,8 @@ class EventsPage extends Component {
   }
 
   pastEvents = ()=>{
-    const {events} = this.props;
-    let currentDate = new Date();
+      const {events} = this.props;
+      let currentDate = new Date();
       //Past Events
       return events.filter(function(event){
         const eventDate = new Date(event.date);
@@ -67,20 +61,20 @@ class EventsPage extends Component {
                 
                  {this.futureEvents().map(event => 
                 <Col xs="4" className="mt-3" key={event._id}>
-                <CardGroup>
-                  <Card>
-                    <CardImg top width="100%" src="https://source.unsplash.com/collection/190727/1600x900" alt="Card image cap" />
-                    <CardBody>
-                      <CardTitle> Event Name:{event.title} </CardTitle>
-                      <CardSubtitle> Location:{event.location} </CardSubtitle>
-                      <CardSubtitle> Date:{event.date} </CardSubtitle>
-                      <CardSubtitle> Sponsors:{event.sponsors}</CardSubtitle>
-                      <CardSubtitle> Chapter:{event.chapter}</CardSubtitle>
-                      <CardText>Description:{event.description.substr(0,50)} </CardText>
-                      <Button color="info" onClick = {this.handleClick()} > More info</Button>
-                    </CardBody>
-                  </Card> 
-                  </CardGroup>
+                  <CardGroup>
+                    <Card>
+                      <CardImg top width="100%" src="https://source.unsplash.com/collection/190727/1600x900" alt="Card image cap" />
+                      <CardBody>
+                        <CardTitle> Event Name:{event.title} </CardTitle>
+                        <CardSubtitle> Location:{event.location} </CardSubtitle>
+                        <CardSubtitle> Date:{event.date} </CardSubtitle>
+                        <CardSubtitle> Sponsors:{event.sponsors}</CardSubtitle>
+                        <CardSubtitle> Chapter:{event.chapter}</CardSubtitle>
+                        <CardText>Description:{event.description.substr(0,50)} </CardText>
+                        <Button color="info" onClick = {()=> this.handleClick(event._id)} > More info</Button>
+                      </CardBody>
+                    </Card> 
+                   </CardGroup>
                 </Col>
                 )}
             </Row>
@@ -98,7 +92,7 @@ class EventsPage extends Component {
                       <CardSubtitle> Sponsors:{event.sponsors}</CardSubtitle>
                       <CardSubtitle> Chapter:{event.chapter}</CardSubtitle>
                       <CardText>Description:{event.description.substr(0,50)} </CardText>
-                      <Button color="info" > More info</Button>
+                      <Button color="info" onClick = {()=> this.handleClick(event._id)}> More info</Button>
                     </CardBody>
                   </Card> 
                   </CardGroup>
@@ -116,4 +110,4 @@ function mapStateToProps(state){
     }
 }
 
-export default connect(mapStateToProps, {getEvents})(EventsPage);
+export default connect(mapStateToProps, {getEvents})(withRouter(EventsPage));
